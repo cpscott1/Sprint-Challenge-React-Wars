07cb49dca3f0a5c971998f6437872e968925ef6f
cpscott1 <thacam18@gmail.com>
2019-03-09 13:59:02 -0600

    finished answers

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -3,9 +3,9 @@ import React from "react";
 export default function Character(props) {
   return (
     <div className="character">
-      <h2>Name: {props.character.name}</h2>
-      <p>Gender: {props.character.gender}</p>
-      <p>Height: {props.character.height}</p>
+      <h2>{props.character.name}</h2>
+      <p><strong>Gender:</strong> {props.character.gender}</p>
+      <p><strong>Height:</strong> {props.character.height}</p>
     </div>
   )
 }
