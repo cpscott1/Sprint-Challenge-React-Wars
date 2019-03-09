61402dafaf6e4ba3d838f21f9cf7f06576b80390
cpscott1 <thacam18@gmail.com>
2019-03-09 11:56:08 -0600

    added props to data

---
+++ starwars/src/components/Character.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Character(props) {
+  return (
+    <div className="character">
+      <p>character={props.character.name}</p>
+    </div>
+  )
+}
